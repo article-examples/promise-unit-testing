e4bde0b63aff32ec5401e38b2547e5a18ebac5c5
Venkatraman <ramsunvtech@gmail.com>
2017-12-22 14:16:58 +0530

    Promise Unit Testing

--- snippets/sinon.js
+++ snippets/sinon.js
@@ -2,7 +2,7 @@ const dbStub = sinon.stub(db, 'getData').callsFake(() => {
     return Promise.resolve(response);
 });
 
-const dbStub = sinon.spy(db, 'getData');
+const dbSpyy = sinon.spy(db, 'getData');
 expect(db.getData(0).args[0]).to.deep.equal({
     success: true,
-})
+});
